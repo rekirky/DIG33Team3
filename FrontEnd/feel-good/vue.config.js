57053638a0d9741b18383e24c47b55c0fb1cc644
WestonCode <contact@westoncode.com>
2019-06-16 22:31:36 +1000

    Added 404 route

--- FrontEnd/feel-good/vue.config.js
+++ FrontEnd/feel-good/vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: config => {
+    config.module
+      .rule('svg')
+      .use('file-loader')
+      .loader('svg-sprite-loader')
+  }
 }
