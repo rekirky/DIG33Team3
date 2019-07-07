9cd2d7d28ea72f9c7aa708fe129a7e55f60fb328
WestonCode <contact@westoncode.com>
2019-07-07 18:15:45 +1000

    Added Details to Feel Section

--- FrontEnd/feel-good/src/views/Home.vue
+++ FrontEnd/feel-good/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
 
-    <div class="feel-container third-container" @mouseenter="bottomBarGrow" @mouseleave="bottomBarShrink">
+    <div id="feel-container" class="third-container" @mouseenter="bottomBarGrow" @mouseleave="bottomBarShrink" @click="feelActive">
       <div class="top-bar"></div>
       <div class="center-content">
         <div class="top-tag">100%</div>
@@ -14,6 +14,36 @@
       </div>
       <div class="bottle right-bottle">
         <img src="@/assets/img/bottles/Paper-Bottle_pom_499.png">
+      </div>
+      <div class="hand">
+        <img src="@/assets/img/hand_1.png">
+      </div>
+    </div>
+    <div class="feel-content-container">
+      <div id="feel-content">
+        <div id="close-button" v-on:click="feelDeactive">
+          <div class="circle hamburger">
+            <span class="bar top"></span>
+            <span class="bar bottom"></span>
+          </div>
+        </div>
+        <div class="two-third-container">        
+          <div class="two-third-title">
+            We want you to feel the benifits of healthy
+            living, for you and future generations.
+          </div>
+          <div class="two-third-body">
+            Ti qui a nonsectaepre con con con est peliqui dolupti ncimus ea qui resequis volor auda
+            pliberae nat veribus, sed molupti volorest, secab ipiscii squaspeliam et eiuntem eosti
+            in nonsent di dolore, qui aligenestet alic tecto cum inis aborro qui dus sum diat latquia
+            simendam coressi musciet archicidel earcium que cus et ulla sequis reici dendebit quis qui
+            sant aliquid erspici pienimi, consequ atecaec temporio.<br><br>
+            Et fuga. Fictota tiatectet aut assecessum quae liqui blaboreped et ut aliquaero consed
+            quuntecto blatur, conserit omnis pe reprae veliciis poresti dis doluptur rent lam fugit
+            venecte molorep editaquatur architi dolent omnisimet eatus, ex elibus dus volorpo repudae.
+          </div>        
+          <router-link to="products" tag="button">The Benefits</router-link>
+        </div>
       </div>
     </div>
 
@@ -68,6 +98,28 @@ export default {
     },
     bottomBarShrink(event){
       event.target.getElementsByClassName("bottom-bar")[0].classList.remove("grow")
+    },
+    feelActive(){
+      var feelContainer = document.getElementById("feel-container")
+      feelContainer.getElementsByClassName("bottom-bar")[0].classList.add("active")
+      feelContainer.getElementsByClassName("top-tag")[0].classList.add("active")
+      feelContainer.getElementsByClassName("bottom-tag")[0].classList.add("active")
+      feelContainer.getElementsByClassName("name-tag")[0].classList.add("active")
+      feelContainer.getElementsByClassName("left-bottle")[0].classList.add("active")
+      feelContainer.getElementsByClassName("right-bottle")[0].classList.add("active")
+      feelContainer.getElementsByClassName("hand")[0].classList.add("active")
+      document.getElementById("feel-content").classList.add("active")
+    },
+    feelDeactive(){      
+      var feelContainer = document.getElementById("feel-container")
+      feelContainer.getElementsByClassName("bottom-bar")[0].classList.remove("active")
+      feelContainer.getElementsByClassName("top-tag")[0].classList.remove("active")
+      feelContainer.getElementsByClassName("bottom-tag")[0].classList.remove("active")
+      feelContainer.getElementsByClassName("name-tag")[0].classList.remove("active")
+      feelContainer.getElementsByClassName("left-bottle")[0].classList.remove("active")
+      feelContainer.getElementsByClassName("right-bottle")[0].classList.remove("active")
+      feelContainer.getElementsByClassName("hand")[0].classList.remove("active")
+      document.getElementById("feel-content").classList.remove("active")
     }
   }
 }
@@ -106,16 +158,29 @@ export default {
   }
 
   .bottom-bar.grow{
-    height: 20%;
+    height: 17%;
+  }
+
+  .bottom-bar.active{
+    height: 100%;
   }
 
   .bottle{
     position: absolute;
     bottom: -10%;
+    transition: all ease-out 1s;
+  }
+
+  .bottle.active{
+    z-index: 2;
   }
 
   .bottle>img{
     height: 50vh;
+  }
+
+  .bottle.active>img{
+    height: 70vh;
   }
 
   .left-bottle{
@@ -123,12 +188,22 @@ export default {
     transform: rotate(-10deg)
   }
 
+  .left-bottle.active{
+    left: -70px;
+    transform: rotate(0deg)
+  }
+
   .right-bottle{
     right: 30%;
     transform: rotate(10deg)
   }
+
+  .right-bottle.active{
+    right: -70px;
+    transform: rotate(0deg)
+  }
   
-  .feel-container{
+  #feel-container{
     position: absolute;
     top: 0;
     left: 0;
@@ -136,8 +211,37 @@ export default {
     width: 33%;
   }
 
-  .feel-container .top-bar, .feel-container .bottom-bar{
+  #feel-container .top-bar, #feel-container .bottom-bar{
     background-color: var(--blue-secondary);
+  }
+
+  .feel-content-container{
+    width: 67%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 33%;
+    overflow: hidden;
+  }
+
+  #feel-content{
+    background-image: url('../assets/img/baby-beautiful.png');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    min-height: 100%;  
+    z-index: 1;
+    transition: all ease-out 1s;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: -100%;
+    opacity: 1;
+    display: flex;
+  }
+
+  #feel-content.active{
+    left: 0;
   }
 
   .natural-container{
@@ -170,6 +274,8 @@ export default {
     font-family: 'Holtwood One SC', sans-serif;
     font-size: 6rem;
     line-height: 1;
+    transition: all ease-out .3s;
+    user-select: none;
   }
 
   .name-tag{
@@ -179,6 +285,8 @@ export default {
     font-weight: 700;
     line-height: 1;
     padding-bottom: 1rem;
+    transition: all ease-out .3s;
+    user-select: none;
   }
 
   .bottom-tag{
@@ -186,8 +294,13 @@ export default {
     font-family: 'Open Sans', sans-serif;
     font-size: 2.2rem;
     font-weight: 700;
+    transition: all ease-out .3s;
+    user-select: none;
   }
 
+  .bottom-tag.active, .top-tag.active, .name-tag.active{
+    color: white;
+  }
 
   .bottle>img:hover {
     animation: jiggle ease-in-out .7s;
@@ -201,6 +314,80 @@ export default {
     60% { transform: rotate(10deg); }
     80% { transform: rotate(-10deg); }
     100% { transform: rotate(0deg); }
+  }
+
+  #close-button{
+    background-color: white;
+    border: 2px solid var(--orange-primary);
+    display: inline-block;
+    border-radius: 500px;
+    top: 2.5rem; 
+    right: 2rem;
+    position: absolute;
+    padding: .5rem;
+    cursor: pointer;
+    z-index: 4;
+  }
+
+  .circle{
+    width: 1.5rem;
+    height: 1.5rem;
+    position: relative;
+  }
+
+  .bar{
+    position: absolute;
+    left: 0;
+    height: 0.2rem;
+    width: 100%;
+    background-color: var(--orange-primary);
+    border-radius: 2px;
+    transition: all cubic-bezier(0.26, 0.1, 0.27, 1.5) 0.35s;
+    top: 48%;
+  }
+
+  .top{
+    transform: rotate(135deg);
+  }
+
+  .bottom{
+    transform: rotate(-135deg);
+  }
+
+  .two-third-container{
+    margin-top: 50%;
+    min-height: 50%;
+    width: 100%;
+    position: relative;
+    background-color: rgba(255,255,255,0.9);
+    padding: 0px 120px 0px 100px;
+  }
+
+  .two-third-title{
+    font-family: 'Open Sans', sans-serif;
+    font-size: 3rem;
+    color: var(--orange-primary);
+    padding: 10px 0px;
+  }
+
+  .two-third-body{
+    font-family: 'Open Sans', sans-serif;
+    text-align: left;
+    font-size: 1.1rem;
+    padding: 5px 0px;
+    line-height: 1.3;
+  }
+
+  .hand{
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translate(-50%, 100%);
+  }
+
+  .hand.active>img{
+    transition: all ease-in .5s;
+    transform: translate(0, -100%);
   }
 
 </style>
