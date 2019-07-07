ac4d7c32fdb8692473a4604a2d2b553eb3328494
Kim-H-2019 <51436393+Kim-H-2019@users.noreply.github.com>
2019-07-07 16:15:05 +1000

    Update About.vue

--- FrontEnd/feel-good/src/views/About.vue
+++ FrontEnd/feel-good/src/views/About.vue
@@ -1,18 +1,56 @@
 <template>
+
   <div class="about">
     <header>
-    <h2> THE HEART OF OUR COMPANY
-    </h2>
-     <h1>LET'S TALK ABOUT US
-    </h1>
+      <!-- Bootstrap classes to style elements -->
+      <section class="about-header d-flex align-item-center text-center">       
+      <div class=" container">
+    <h2> THE HEART OF OUR COMPANY</h2>
+     <h1>LET'S TALK ABOUT US</h1>
+      </div>
+      </section>
     </header>
-    <article> 
-      <Section>
-        <p> </p>
+
+    <article class="about-main"> 
+      <Section class="about-text d-flex align-item-center text-center">
+    <div class="container">
+        <h3>Title text</h3>
+        <p>paragraph text</p>
+     </div>
       </Section>
-    <Section>
-        
+
+    <Section class="about-images d-flex align-items-center"> 
+
+      <div class="container">
+        <img src="@/assets/img/sm_logo.png" alt="feel good drinks logo small">
+      </div>
+
+       <!--<div class="container">-->
+        <!-- Bootstrap grid for image layout/breakpoints -->
+      <div id="about-image-grid" class="">
+        <div class="image-grid">
+          <div class="row justify-content-center">
+        <div class="col-12 col-sm-4 col-md-6 col-lg-6">
+        <img src="@/assets/img/footprint.png" alt="footprint on beach image">
+          </div>
+
+        <div class="col-12 col-sm-4 col-md-6 col-lg-6">
+        <img src="@/assets/img/family.png" alt="family at beach image">
+          </div>
+
+          <div class="col-12 col-sm-4 col-md-6 col-lg-6">
+        <img src="@/assets/img/bottles_1.png" alt="multiple drink bottle image">
+          </div> 
+
+          <div class="col-12 col-sm-4 col-md-6 col-lg-6">     
+        <img src="@/assets/img/beach.png" alt="aerial beach view image">
+          </div>
+          </div>
+        </div>
+        </div>
+        <!--</div> -->
       </Section>
+
     </article>
   </div>
 </template>
