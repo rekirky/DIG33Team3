03d1aedcd338cd2787f1fd797cfafb47e847347f
Kim-H-2019 <51436393+Kim-H-2019@users.noreply.github.com>
2019-07-08 19:08:45 +1000

    About page WIP update

--- FrontEnd/feel-good/src/views/About.vue
+++ FrontEnd/feel-good/src/views/About.vue
@@ -1,58 +1,75 @@
 <template>
 
   <div class="about">
+
+    <article class="about-main"> 
     <header>
-      <!-- Bootstrap classes to style elements -->
+      <!-- Bootstrap and custom classes to style elements -->
       <section class="about-header d-flex align-item-center text-center">       
       <div class="container">
-    <h2> THE HEART OF OUR COMPANY</h2>
-     <h1>LET'S TALK ABOUT US</h1>
+      <h1><small> THE HEART OF OUR COMPANY </small></h1>
+      <h1>LET'S TALK ABOUT US</h1>
       </div>
       </section>
     </header>
 
-    <article class="about-main"> 
+    <section> 
+        <!-- Bootstrap flex box -->
       <Section class="about-text d-flex align-item-center text-center">
+         <!-- Bootstrap container -->
     <div class="container">
-        <h3>Title text</h3>
-        <p>paragraph text</p>
-     </div>
-      </Section>
+      <h3 class="lead">Title text</h3>
+      <p>paragraph text</p>
+      </div>
+    </Section>
 
     <Section class="about-images d-flex align-item-center"> 
      <div class="about-logo container">
         <div class="about-logo-sm">
-        <img src="@/assets/img/sm_logo.png" alt="feel good drinks logo small">
+        <img class="img-fluid float-left" src="@/assets/img/sm_logo.png" alt="feel good drinks logo small">
         </div>
-      </div>
+          </div>
 
-       <div class="container">
-        <!-- Bootstrap grid for image layout/breakpoints -->
+       <div class="image-grid container">
+        <!-- Bootstrap grid for image layout -->
       <div id="about-image-grid">
         <div class="image-grid">
-          <div class="row justify-content-center">
-        <div class="about-image col-6 ">
-        <img src="@/assets/img/footprint.png" alt="footprint on beach image">
+        <div class="row justify-content-center">
+        <div class="about-image col-6">
+        <img class="img-fluid" src="@/assets/img/footprint.png" alt="footprint on beach image">
           </div>
-
-        <div class="about-image col-6 ">
-        <img src="@/assets/img/family.png" alt="family at beach image">
+        <div class="about-image col-6">
+        <img class="img-fluid" src="@/assets/img/family.png" alt="family at beach image">
           </div>
-
-          <div class="about-image col-6 ">
-        <img src="@/assets/img/bottles_1.png" alt="multiple drink bottle image">
+        <div class="about-image col-6">
+        <img class="img-fluid"src="@/assets/img/bottles_1.png" alt="multiple drink bottle image">
           </div> 
-
-          <div class="about-image col-6 ">     
-        <img src="@/assets/img/beach.png" alt="aerial beach view image">
+        <div class="about-image col-6">     
+        <img class="img-fluid" src="@/assets/img/beach.png" alt="aerial beach view image">
           </div>
+            </div>
+              </div>
+                </div>
+                  </div>
+        <div class="side-bar container">
           </div>
-        </div>
-        </div>
-        </div>
-      </Section>
-
+           </section>
+              </section>
     </article>
+           
+
+<section>
+    <div class="consumer-notice d-flex align-items-left">
+       <div class="consumer-logo">     
+        <img class="img-fluid float-left" src="@/assets/img/logo_247.png" alt="feel good drinks logo">
+          </div> 
+          <div class="consumer-info">
+            <h5 class="text-light text-left">Consumer Notice</h5>
+            <p class="text-light text-left">Buy today</p>
+              </div>
+                </div>
+  </section>
+
   </div>
 </template>
 
@@ -67,24 +84,42 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 
-
 .about-logo.container {
-  width: 40%;
-  margin-top: 50%;
+  margin-top: 40%;
+  width: 20%;
+  border: 10px
 }
 
+.image-grid.container {
+  width:60%
+}
+
+.side-bar.container {
+  width:20%
+}
 
 .about-image img{
-  width: 100%;
-  height: auto;
   margin: 10px
 }
 
 .about-logo-sm img{
   margin: 10px;
-  margin-right: 180px
+  margin-right: 220px
 }
 
+.consumer-logo{
+padding: 40px
+}
+
+.consumer-notice{
+background-color: var(--purple-primary);
+height: 350px;
+margin-top: 20px
+}
+
+.consumer-info{
+padding:40px 20px;
+}
 
 </style>
 
