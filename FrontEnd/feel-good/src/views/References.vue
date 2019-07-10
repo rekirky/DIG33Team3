cf3e9613cef3ee5467e44e3e7c844a32fb0002ec
Kim-H-2019 <51436393+Kim-H-2019@users.noreply.github.com>
2019-07-10 20:15:13 +1000

    Added 3rd party content and build tool references to reference page

--- FrontEnd/feel-good/src/views/References.vue
+++ FrontEnd/feel-good/src/views/References.vue
@@ -1,24 +1,61 @@
 <template>
   <div class="references">
 
-  <section class="reference-list align-item-left text-center">
-    <h2> <b>References </b></h2>
+  <section class="reference-list align-item-center text-center">
+    <h2><b>References </b></h2>
 
       <div>
-        <h4> <b>Build tools, frameworks and libraries </b></h4>
+        <h4><b>Build tools, frameworks and libraries</b></h4>
           <p>   
+            Abobe CC Illustrator - https://www.adobe.com/au/products/illustrator.html.<br>
+            Adobe CC Photoshop - https://www.adobe.com/au/products/photoshop.html.<br>
+            Amazon Lightsail - https://lightsail.aws.amazon.com/.<br>
+            Bootstrap -  https://getbootstrap.com/.<br>
+            Composer - https://getcomposer.org/.<br>
+            Docker - Docker. 2019. “Docker Icon”. Digital Image. https://docker.com/.<br>
+            Laravel - https://laravel.com/.<br>
+            Laravel Artisan - https://www.laravel-artisan.com/.<br>
+            Lumen - https://lumen.laravel.com/.<br>
+            MySQL - https://mysql.com/.<br>
+            Netlify - https://netlify.com/.<br>
+            jQuery - https://jquery.com/. <br>
+            Vue - https://Vuejs.org/. <br>
             </p>
        </div>
 
       <div>
        <h4><b>Fonts</b></h4>
           <p> 
+            Matteson Steve. 2019. OpenSans. Google Fonts, https://fonts.google.com/specimen/Open+Sans.<br>
+            Font Awesome. 2019. Social media icons. https://fontawesome.com/.<br>
+            Vernon Adams. 2011. Holtwood One SC. Google Fonts, https://fonts.google.com/specimen/Holtwood+One+SC.<br>
             </p>
       </div>
 
      <div>
         <h4><b>Images</b></h4>
-          <p>
+          <p>   
+            Adehoidar. 2016.  ‘Fruit illustrations’. https://creativemarket.com/adehoidar/692848-Fruit-illustrations. Accessed 20 June 2019. <br>
+            Aweeka. 2019.  ‘Mango and oranges’https://www.shutterstock.com/image-photo/oranges-mango-46109116?. Accessed 20 June 2019.<br>
+            Bacin. 2013. ‘Passionfruit’ . https://www.istockphoto.com/au/photo/passion-fruit-gm181128185-25663433. Accessed 20 June 2019. <br>
+            Boltneva, Valeria. 2019. ‘Photo of Foot Print on the Seashore’. https://www.pexels.com/photo/photo-of-foot-print-on-the-seashore-939046/. Accessed 20 June 2019.<br>
+            Brown, Deborah. 2019. ‘Feel Good Drinks Paper Bottles’. <br>
+            Brown, Deborah. 2019. Hand illustrations, fish and plants.<br>
+            Brown, Deborah. 2019. ‘3D world illustration’. <br>
+            Creation Hill.  2019. Pexels.com. ‘Baby, beautiful, carefree’. https://www.pexels.com/photo/woman-carrying-baby-near-grass-1261909/. Accessed 20 June 2019. <br>
+            Diamant24.  2019. ‘Mango’. https://www.123rf.com/photo_16258723_ripe-red-mango-fruit-on-white-background.html. Accessed 20 June 2019.<br>
+            Eriksen M, Lebreton LCM, Carson HS, Thiel M, Moore CJ, et al. 2014. Plastic Pollution in the World’s Oceans: More than 5 Trillion PlasticPieces Weighing over 250,000 Tons Afloat at Sea. PLoS ONE 9(12): e111913. doi:10.1371/journal.pone.0111913. <br>
+            Feel Good Drinks Co. 2019. Feel Good Drinks Co. logo. http://www.feelgooddrinks.co.uk/. Accessed 10 June 2019. <br>
+            ImgBin. 2019. ‘Elderberry flower’. https://imgbin.com/png/tYxr8eYc/elderflower-cordial-elderberry-shrub-lemon-syrup-png. Accessed 20 June 2019. <br>
+            Hellgram. 2017. Fruits patterns set. https://creativemarket.com/Hellgram/1358737-Fruits-patterns-set. Accessed 20 June 2019. <br>
+            Juliann. 2019. ‘Political map of the world. Gray world map-countries. Vector illustration - Vector'.  https://www.shutterstock.com/image-vector/political-map-world-gray-mapcountries-vector-545606905?studio=1. Accessed 20 June 2019. <br>
+            Lukas. 2019. ‘Lemons’. https://www.pexels.com/photo/close-up-photo-of-sliced-yellow-lemon-on-white-surface-1414110/. Accessed 20 June 2019.
+            Mr. Anaked. 2019. Plastic bottle in the ocean sea water – Image. https://www.shutterstock.com/image-photo/plastic-bottle-ocean-sea-water-1101428288?. Accessed 20 June 2019. <br>
+            Pixelbay. 2019. ‘Blue Body Water’. https://www.pexels.com/photo/beach-foam-landscape-nature-533923/. Accessed 20 June 2019.<br>
+            Pixelbay. 2019. ‘White and Gray Dolphin on Blue Water’. https://www.pexels.com/photo/white-and-gray-dolphin-on-blue-water-64219/. Accessed 20 June 2019. <br>
+            Pngimg.com. 2019. 'Pomegranate’ https://www.crazypng.com/18-pomegranate-png-images-downloaded-free-charge.html. Accessed 20 June 2019. <br>
+            Rawpixel.com. 2019. ‘Blue Shorts’. https://www.pexels.com/photo/barefoot-beach-blur-break-296879/. Accessed 20 June 2019. <br>
+            Rawpixel.com. 2018. ‘Photo of Family On Seashore’ https://www.pexels.com/photo/photo-of-family-on-seashore-1574653/. Accessed 20 June 2019.<br>
             </p>
       </div>
 </section>
@@ -38,14 +75,30 @@ export default {
 <style scoped lang="scss">
 
 .reference-list{
- width:80%;
- margin: 0 10% 0 10%
+ width:60%;
+ margin: 0 20% 10% 20%
 }
 
 h2{
   color: var(--orange-primary);
   margin-top: 40px;
   padding: 10px
+}
+
+h4{
+text-align: center;
+}
+
+
+p {
+  line-height: 1.8;
+  display: block;
+  text-align: left;
+  margin-top: 1em;
+  margin-bottom: 1 em;
+  margin-left: 0;
+  margin-right: 0;
+  padding-bottom: 40px;
 }
 
 </style>
