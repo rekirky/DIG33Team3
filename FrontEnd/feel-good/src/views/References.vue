1a87892c9f1d184c2152ed0017c11c91599578d8
Kim-H-2019 <51436393+Kim-H-2019@users.noreply.github.com>
2019-07-10 18:09:06 +1000

    Added reference page titles including styling

--- FrontEnd/feel-good/src/views/References.vue
+++ FrontEnd/feel-good/src/views/References.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="references">
-    references
+
+  <section class="reference-list align-item-left text-center">
+    <h2> <b>References </b></h2>
+
+      <div>
+        <h4> <b>Build tools, frameworks and libraries </b></h4>
+          <p>   
+            </p>
+       </div>
+
+      <div>
+       <h4><b>Fonts</b></h4>
+          <p> 
+            </p>
+      </div>
+
+     <div>
+        <h4><b>Images</b></h4>
+          <p>
+            </p>
+      </div>
+</section>
+   
   </div>
 </template>
 
@@ -14,5 +36,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
+.reference-list{
+ width:80%;
+ margin: 0 10% 0 10%
+}
+
+h2{
+  color: var(--orange-primary);
+  margin-top: 40px;
+  padding: 10px
+}
 
 </style>
