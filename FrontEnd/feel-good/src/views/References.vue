7d8b2d82af817700b87d7623d53a007eded76657
Kim-H-2019 <51436393+Kim-H-2019@users.noreply.github.com>
2019-07-15 12:40:40 +1000

    Updated reference list spacing

--- FrontEnd/feel-good/src/views/References.vue
+++ FrontEnd/feel-good/src/views/References.vue
@@ -1,65 +1,67 @@
 <template>
   <div class="references">
 
-  <section class="reference-list align-item-center text-center">
-    <div class="container-fluid">
+  <!-- Bootstrap and custom classes to style elements -->
+  <section class="reference-list text-center">
+    <div class="container">
       
     <h2>References</h2>
 
       <div>
         <h6>Build tools, frameworks and libraries</h6>
           <ul>   
-           <li> Abobe CC Illustrator - https://www.adobe.com/au/products/illustrator.html.</li>
+           <li> Adobe CC Illustrator - https://www.adobe.com/au/products/illustrator.html.</li>
            <li> Adobe CC Photoshop - https://www.adobe.com/au/products/photoshop.html.</li>
            <li> Amazon Lightsail - https://lightsail.aws.amazon.com/.</li>
            <li> Bootstrap -  https://getbootstrap.com/.</li>
            <li> Composer - https://getcomposer.org/.</li>
-           <li>Docker - Docker. 2019. “Docker Icon”. Digital Image. https://docker.com/.</li>
+           <li>Docker -  https://docker.com/.</li>
            <li>Laravel - https://laravel.com/.</li>
            <li> Laravel Artisan - https://www.laravel-artisan.com/.</li>
            <li> Lumen - https://lumen.laravel.com/.</li>
            <li> MySQL - https://mysql.com/.</li>
            <li> Netlify - https://netlify.com/.</li>
            <li> NPM - https://www.npmjs.com/ </li>
-           <li>  jQuery - https://jquery.com/. </li>
+           <li> jQuery - https://jquery.com/. </li>
            <li> Vue - https://Vuejs.org/. </li>
             </ul>
        </div>
+  </div> 
 
-      <div>
+      <div class="reference-list-entries container">
        <h6><b>Fonts</b></h6>
-          <ul> 
-           <li> Matteson Steve. 2019. OpenSans. Google Fonts, https://fonts.google.com/specimen/Open+Sans.</li>
-           <li> Font Awesome. 2019. Social media icons. https://fontawesome.com/.<br>
-           <li> Vernon Adams. 2011. Holtwood One SC. Google Fonts, https://fonts.google.com/specimen/Holtwood+One+SC.</li>
-            </ul>
-      </div>
+          <div class="font-references"> 
+           <div> <p class="list-entry"> Matteson Steve. 2019. OpenSans. Google Fonts, https://fonts.google.com/specimen/Open+Sans.</p></div>
+           <div> <p class="list-entry"> Font Awesome. 2019. Social media icons. https://fontawesome.com/.</p></div>
+           <div> <p class="list-entry"> Vernon Adams. 2011. Holtwood One SC. Google Fonts, https://fonts.google.com/specimen/Holtwood+One+SC.</p></div>
+            </div>
+      
 
-     <div>
+     <div class="reference-list-entries clear-fix">
         <h6>Images</h6>
-          <ul>   
-          <li> Adehoidar. 2016.  ‘Fruit illustrations’. https://creativemarket.com/adehoidar/692848-Fruit-illustrations. Accessed 20 June 2019. <br>
-          <li> Aweeka. 2019.  ‘Mango and oranges’https://www.shutterstock.com/image-photo/oranges-mango-46109116?. Accessed 20 June 2019.<br>
-          <li> Bacin. 2013. ‘Passionfruit’ . https://www.istockphoto.com/au/photo/passion-fruit-gm181128185-25663433. Accessed 20 June 2019. <br>
-          <li> Boltneva, Valeria. 2019. ‘Photo of Foot Print on the Seashore’. https://www.pexels.com/photo/photo-of-foot-print-on-the-seashore-939046/. Accessed 20 June 2019.<br>
-          <li> Brown, Deborah. 2019. ‘Feel Good Drinks Paper Bottles’. <br>
-          <li>  Brown, Deborah. 2019. Hand illustrations, fish and plants.<br>
-          <li> Brown, Deborah. 2019. ‘3D world illustration’. <br>
-          <li> Creation Hill.  2019. Pexels.com. ‘Baby, beautiful, carefree’. https://www.pexels.com/photo/woman-carrying-baby-near-grass-1261909/. Accessed 20 June 2019. </li>
-          <li> Diamant24.  2019. ‘Mango’. https://www.123rf.com/photo_16258723_ripe-red-mango-fruit-on-white-background.html. Accessed 20 June 2019.</li>
-          <li> Eriksen M, Lebreton LCM, Carson HS, Thiel M, Moore CJ, et al. 2014. Plastic Pollution in the World’s Oceans: More than 5 Trillion PlasticPieces Weighing over 250,000 Tons Afloat at Sea. PLoS ONE 9(12): e111913. doi:10.1371/journal.pone.0111913. </li>
-          <li> Feel Good Drinks Co. 2019. Feel Good Drinks Co. logo. http://www.feelgooddrinks.co.uk/. Accessed 10 June 2019. </li>
-          <li> ImgBin. 2019. ‘Elderberry flower’. https://imgbin.com/png/tYxr8eYc/elderflower-cordial-elderberry-shrub-lemon-syrup-png. Accessed 20 June 2019. </li>
-          <li> Hellgram. 2017. Fruits patterns set. https://creativemarket.com/Hellgram/1358737-Fruits-patterns-set. Accessed 20 June 2019. </li>
-          <li> Juliann. 2019. ‘Political map of the world. Gray world map-countries. Vector illustration - Vector'.  https://www.shutterstock.com/image-vector/political-map-world-gray-mapcountries-vector-545606905?studio=1. Accessed 20 June 2019. </li>
-          <li> Lukas. 2019. ‘Lemons’. https://www.pexels.com/photo/close-up-photo-of-sliced-yellow-lemon-on-white-surface-1414110/. Accessed 20 June 2019.</li>
-          <li> Mr. Anaked. 2019. Plastic bottle in the ocean sea water – Image. https://www.shutterstock.com/image-photo/plastic-bottle-ocean-sea-water-1101428288?. Accessed 20 June 2019. </li>
-          <li> Pixelbay. 2019. ‘Blue Body Water’. https://www.pexels.com/photo/beach-foam-landscape-nature-533923/. Accessed 20 June 2019.</li>
-          <li> Pixelbay. 2019. ‘White and Gray Dolphin on Blue Water’. https://www.pexels.com/photo/white-and-gray-dolphin-on-blue-water-64219/. Accessed 20 June 2019. </li>
-          <li> Pngimg.com. 2019. 'Pomegranate’ https://www.crazypng.com/18-pomegranate-png-images-downloaded-free-charge.html. Accessed 20 June 2019. </li>
-          <li>Rawpixel.com. 2019. ‘Blue Shorts’. https://www.pexels.com/photo/barefoot-beach-blur-break-296879/. Accessed 20 June 2019. </li>
-          <li> Rawpixel.com. 2018. ‘Photo of Family On Seashore’ https://www.pexels.com/photo/photo-of-family-on-seashore-1574653/. Accessed 20 June 2019.</li>
-            </ul>
+          <div class="image-references">       
+          <div> <p class="list-entry"> Adehoidar. 2016.  ‘Fruit illustrations’. https://creativemarket.com/adehoidar/692848-Fruit-illustrations. Accessed 20 June 2019.</p></div>
+          <div> <p class="list-entry"> Aweeka. 2019.  ‘Mango and oranges’. https://www.shutterstock.com/image-photo/oranges-mango-46109116?. Accessed 20 June 2019.</p></div>
+          <div> <p class="list-entry"> Bacin. 2013. ‘Passionfruit’. https://www.istockphoto.com/au/photo/passion-fruit-gm181128185-25663433. Accessed 20 June 2019.</p></div>
+          <div> <p class="list-entry"> Boltneva, Valeria. 2019. ‘Photo of Foot Print on the Seashore’. https://www.pexels.com/photo/photo-of-foot-print-on-the-seashore-939046/. Accessed 20 June 2019.</p></div>
+          <div> <p class="list-entry"> Brown, Deborah. 2019. ‘Feel Good Drinks Paper Bottles’.</p> </div>
+          <div> <p class="list-entry"> Brown, Deborah. 2019. Hand illustrations, fish and plants.</p></div>
+          <div> <p class="list-entry"> Brown, Deborah. 2019. ‘3D world illustration’. </p></div>
+          <div> <p class="list-entry"> Creation Hill.  2019. Pexels.com. ‘Baby, beautiful, carefree’. https://www.pexels.com/photo/woman-carrying-baby-near-grass-1261909/. Accessed 20 June 2019.</p> </div>
+          <div> <p class="list-entry"> Diamant24.  2019. ‘Mango’. https://www.123rf.com/photo_16258723_ripe-red-mango-fruit-on-white-background.html. Accessed 20 June 2019. </p></div>
+          <div> <p class="list-entry"> Eriksen M, Lebreton LCM, Carson HS, Thiel M, Moore CJ, et al. 2014. Plastic Pollution in the World’s Oceans: More than 5 Trillion PlasticPieces Weighing over 250,000 Tons Afloat at Sea. PLoS ONE 9(12): e111913. doi:10.1371/journal.pone.0111913. </p></div>
+          <div> <p class="list-entry"> Feel Good Drinks Co. 2019. Feel Good Drinks Co. logo. http://www.feelgooddrinks.co.uk/. Accessed 10 June 2019. </p></div>
+          <div> <p class="list-entry"> ImgBin. 2019. ‘Elderberry flower’. https://imgbin.com/png/tYxr8eYc/elderflower-cordial-elderberry-shrub-lemon-syrup-png. Accessed 20 June 2019. </p> </div>
+          <div> <p class="list-entry"> Hellgram. 2017. Fruits patterns set. https://creativemarket.com/Hellgram/1358737-Fruits-patterns-set. Accessed 20 June 2019. </p> </div>
+          <div> <p class="list-entry"> Juliann. 2019. ‘Political map of the world. Gray world map-countries. Vector illustration - Vector'. https://www.shutterstock.com/image-vector/political-map-world-gray-mapcountries-vector-545606905?studio=1. Accessed 20 June 2019. </p></div>
+          <div> <p class="list-entry"> Lukas. 2019. ‘Lemons’. https://www.pexels.com/photo/close-up-photo-of-sliced-yellow-lemon-on-white-surface-1414110/. Accessed 20 June 2019. </p></div>
+          <div> <p class="list-entry"> Mr. Anaked. 2019. Plastic bottle in the ocean sea water – Image. https://www.shutterstock.com/image-photo/plastic-bottle-ocean-sea-water-1101428288?. Accessed 20 June 2019. </p></div>
+          <div> <p class="list-entry"> Pixelbay. 2019. ‘Blue Body Water’. https://www.pexels.com/photo/beach-foam-landscape-nature-533923/. Accessed 20 June 2019.</p> </div>
+          <div> <p class="list-entry"> Pixelbay. 2019. ‘White and Gray Dolphin on Blue Water’. https://www.pexels.com/photo/white-and-gray-dolphin-on-blue-water-64219/. Accessed 20 June 2019.</p></div>
+          <div> <p class="list-entry"> Pngimg.com. 2019. 'Pomegranate’ https://www.crazypng.com/18-pomegranate-png-images-downloaded-free-charge.html. Accessed 20 June 2019. </p> </div>
+          <div> <p class="list-entry"> Rawpixel.com. 2019. ‘Blue Shorts’. https://www.pexels.com/photo/barefoot-beach-blur-break-296879/. Accessed 20 June 2019. </p> </div>
+          <div> <p class="list-entry"> Rawpixel.com. 2018. ‘Photo of Family On Seashore’ https://www.pexels.com/photo/photo-of-family-on-seashore-1574653/. Accessed 20 June 2019. </p></div>
+            </div>
       </div>
 
       <div>
@@ -68,17 +70,14 @@
            <li> FreePrivacyPolicy.com. 2019. Create a Free Privacy Policy. https://app.freeprivacypolicy.com/builder/download/retrieve?token=e1ea5d874ab4a48bc805b108d23b9fdb. Accessed 11 July 2019</li>        
             </ul>
       </div>
-
-              
- </div> <!--end container-->
-</section> <!--end reference list-->
-
+  </div>             
+</section> 
 
 <section>
     <div class="student-notice">
         <div class="container-fluid">
           <h6 class="notice-header">Student Project Notice</h6>
-            <p><em>This website has been created as part of an assignment in an approved course of study for Curtin University 
+            <p class="notice-text"><em>This website has been created as part of an assignment in an approved course of study for Curtin University 
               and contains copyright material not created by the authors. All copyright material used remains copyright of the
               respective owners and has been used here pursuant to Section 40 of the Copyright Act 1968 (Commonwealth of Australia). 
               No part of this work may be reproduced without consent of the original copyright owners. 
@@ -87,7 +86,6 @@
                   </div>
     </section> <!--end student notice section-->
  
-   
   </div>
 </template>
 
@@ -103,7 +101,6 @@ export default {
 <style scoped lang="scss">
 
 .reference-list{
- width:60%;
  margin: 0 20% 10% 25%
 }
 
@@ -115,9 +112,9 @@ h2{
 }
 
 h6{
-font-weight: bold;
-text-align: center;
-margin-top: 30px
+  font-weight: bold;
+  text-align: center;
+  margin-top: 30px  
 }
 
 ul {
@@ -141,11 +138,27 @@ li a{
 
 h6.notice-header{
  color: var(--orange-primary);
- margin-left: 10%
+ text-align: center;
 }
 
+p.notice-text{
+  width: 70%;
+  margin-right:20%;
+  text-align: justify
+}
+
+p.list-entry{
+  margin:0;
+  padding-bottom: 15px
+}
+
+p.product-entries{
+  margin-top:30px;
+  margin-bottom: 10px
+}
+
+
 p{
-  width:80%;
   margin-left: 15%;
   margin-bottom: 8%;
   text-align: justify
