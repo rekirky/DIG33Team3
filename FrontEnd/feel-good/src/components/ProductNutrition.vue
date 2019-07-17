2d9f7c4c6a070368f42382549d7e6bbc8017883e
WestonCode <contact@westoncode.com>
2019-07-17 20:51:51 +1000

    Added Nutrition Content

--- FrontEnd/feel-good/src/components/ProductNutrition.vue
+++ FrontEnd/feel-good/src/components/ProductNutrition.vue
@@ -3,7 +3,67 @@
     <div class="row no-gutters">
       <div class="col-11">
         <div class="nutrition-content">
+          <div class="product-name">
+            <h3>{{product.name}}</h3>
+            <p>*100% naturally occuring fruit sugars</p>
+          </div>
 
+          <div class="marker fruit-1-marker">
+            <div class="circle-surround">
+              <div class="circle"></div>
+              <div class="circle-line right">
+                <div class="circle-text">{{product.fruit1Description}}</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="marker fruit-2-marker">
+            <div class="circle-surround">
+              <div class="circle"></div>
+              <div class="circle-line left">
+                <div class="circle-text">{{product.fruit2Description}}</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="marker bottle-top-marker">
+            <div class="circle-surround">
+              <div class="circle"></div>
+              <div class="circle-line left">
+                <div class="circle-text">Innovative paper bottle shape built for functionality and strength</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="marker bottle-mid-marker">
+            <div class="circle-surround">
+              <div class="circle"></div>
+              <div class="circle-line right">
+                <div class="circle-text">80% compostable</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="marker bottle-bottom-marker">
+            <div class="circle-surround">
+              <div class="circle"></div>
+              <div class="circle-line left">
+                <div class="circle-text">A blended combination of wheat straw, bamboo, husks, sugar cane, and/or bulrush.</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="hero-bottle">
+            <img :src="require(`@/assets/img/products/${product.bottleHeroImage}`)" :alt="product.name" class="d-block">
+          </div>
+
+          <div class="fruit fruit-1">
+            <img :src="require(`@/assets/img/products/${product.fruit1Image}`)" :alt="product.name" class="d-block">
+          </div>
+
+          <div class="fruit fruit-2">
+            <img :src="require(`@/assets/img/products/${product.fruit2Image}`)" :alt="product.name" class="d-block">
+          </div>
 
 
           <div class="water-drop-container natural-sugar">
@@ -46,6 +106,11 @@
             </div>
           </div>
 
+          <div class="ingredients">
+            <h5>Ingredients</h5>
+            <p>{{product.ingredients}}</p>
+          </div>
+
 
         </div>
       </div>
@@ -82,7 +147,7 @@ export default {
     width: 100%;
     height: 100vh;
     position: absolute;
-    top: 100%;
+    top: 120%;
     transition: all ease-out 1s;
   }
 
@@ -185,5 +250,124 @@ export default {
     font-size: 1rem;
   }
 
+  h3, h5, p{
+    color: white;
+  }
+
+  .product-name{
+    width: 30%;
+    padding: 50px 80px;
+    text-align: left;
+  }
+
+  .hero-bottle{
+    position: absolute;
+    top: -15%;
+    left: 10%;
+    transform: rotate(25deg);
+  }
+
+  .fruit img{
+    width: 400px;
+  }
+
+  .fruit-1{
+    position: absolute;
+    top: 33%;
+    left: 57%;
+  }
+
+  .fruit-2{
+    position: absolute;
+    top: 10%;
+    left: 67%;
+  }
+
+  .circle{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    height: 25px;
+    width: 25px;
+    border-radius: 50%;
+    background-color: white;
+  }
+
+  .circle-surround{
+    position: relative;
+    height: 50px;
+    width: 50px;
+    border-radius: 50%;
+    border: 4px solid white;
+  }
+
+  .circle-line{
+    position: absolute;
+    top: 50%;
+    transform: translate(0, -50%);
+    width: 200px;
+    height: 5px;
+    background-color: white;
+  }
+
+  .left{
+    left: 50%;
+  }
+
+  .right{
+    right: 50%;
+  }
+
+  .circle-text{
+    position: absolute;
+    width: 200px;
+    padding: 15px;
+    font-size: .9rem;
+    color: white;
+    bottom: 0;
+    left: 50%;
+    transform: translate(-50%, 0);
+  }
+
+  .marker{
+    position: absolute;
+    z-index: 1;
+  }
+
+  .fruit-1-marker{
+    top: 40%;
+    left: 63%;
+  }
+
+  .fruit-2-marker{
+    top: 25%;
+    left: 85%;
+  }
+
+  .bottle-top-marker{
+    top: 10%;
+    left: 37%;
+  }
+
+  .bottle-mid-marker{
+    top: 40%;
+    left: 13%;
+  }
+
+  .bottle-bottom-marker{
+    top: 80%;
+    left: 25%;
+  }
+
+  .ingredients{
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    margin: 0px 50px 50px 0px;
+    width: 20%;
+    height: 20%;
+    text-align: left;
+  }
 
 </style>
