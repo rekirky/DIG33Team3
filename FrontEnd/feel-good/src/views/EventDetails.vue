e6be0ad4ccdca3524ab9b4c36b47238c5647c474
Weston Code <contact@westoncode.com>
2019-07-19 08:21:06 +1000

    Updated comments on events page

--- FrontEnd/feel-good/src/views/EventDetails.vue
+++ FrontEnd/feel-good/src/views/EventDetails.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="event-details">
     <div v-if="this.events.length != 0">
+    <!-- If there are events, each one is added to the website -->
     <div v-for="(event, index) in events" :key="index">
+      <!-- The presentation style is based on the event type, Specific components are swapped out for each-->
       <social-event v-if="event.type == 'social'" v-bind:event="event" :id="'event-detail-' + index"/>
       <video-event v-if="event.type == 'video'" v-bind:event="event" :id="'event-detail-' + index"/>
     </div>
@@ -10,8 +12,11 @@
 </template>
 
 <script>
+//Import for vuex local storage
 import { mapGetters } from 'vuex'
+//Import of types to be accessed via despatches
 import { GET_EVENTS } from '@/store/types'
+//Import of components
 import SocialEvent from '@/components/SocialEvent.vue'
 import VideoEvent from '@/components/VideoEvent.vue'
 
@@ -27,8 +32,8 @@ export default {
     })
   },
   async mounted(){
-    await this.$store.dispatch(GET_EVENTS)
-    
+    //Get event information when the page components is mounted to the application
+    await this.$store.dispatch(GET_EVENTS)    
   }
 }
 </script>
