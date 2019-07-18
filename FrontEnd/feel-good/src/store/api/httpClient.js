6b4e248554537de9ab8d8bd25e2ab84d2f8f114a
Weston Code <contact@westoncode.com>
2019-07-19 07:56:48 +1000

    Updated Comments in httpClient.js

--- FrontEnd/feel-good/src/store/api/httpClient.js
+++ FrontEnd/feel-good/src/store/api/httpClient.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 // axios instance
+// Axios handles the HTTP requests between the front end and the backend api
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:8081',
   timeout: 30000
