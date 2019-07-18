6feb5d22a4abad5d2aa3e26708742c11c487fb70
WestonCode <contact@westoncode.com>
2019-07-18 21:11:01 +1000

    Updated Comments on main.js Page

--- FrontEnd/feel-good/src/main.js
+++ FrontEnd/feel-good/src/main.js
@@ -2,16 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
+
+//Import of Jquery library used under the MIT license, https://jquery.org/license/
 import jQuery from 'jquery'
 global.jQuery = jQuery
 global.$ = jQuery
 
+//Import of Bootstrap used under the MIT license https://getbootstrap.com/docs/4.0/about/license/
 let Bootstrap = require('bootstrap')
 import 'bootstrap/dist/css/bootstrap.css'
 
 
 Vue.config.productionTip = false
 
+//Initialisation of the Application
 new Vue({
   router,
   store,
