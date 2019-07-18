129c8c06d6ea534485a09c4fb7770c030a5ac25c
WestonCode <contact@westoncode.com>
2019-07-18 21:13:43 +1000

    Update Comments in router.js

--- FrontEnd/feel-good/src/router.js
+++ FrontEnd/feel-good/src/router.js
@@ -7,6 +7,8 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
+
+  //Allow locations in the App, all other routes will be sent to the 404 page.
   routes: [
     {
       path: '/',
@@ -21,10 +23,7 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import('./views/About.vue')
     },
     {
       path: '/contact',
@@ -81,6 +80,7 @@ export default new Router({
       component: resolve => require(['@/views/NotFound'], resolve)
     }
   ],
+  //Scroll behavior allows hash targets to operate as expected.
   scrollBehavior: function (to) {
     if (to.hash) {
       return {
