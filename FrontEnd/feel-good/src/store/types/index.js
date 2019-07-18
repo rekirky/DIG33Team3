831db906619734c355b7fd02993cb17c3dba55c2
Weston Code <contact@westoncode.com>
2019-07-19 08:13:59 +1000

    Update Commets in types file

--- FrontEnd/feel-good/src/store/types/index.js
+++ FrontEnd/feel-good/src/store/types/index.js
@@ -1,11 +1,15 @@
+//Getter and Setter Types for Content Blocks
 export const GET_CONTENT_BLOCKS = 'GET_CONTENT_BLOCKS'
 export const SET_CONTENT_BLOCKS = 'SET_CONTENT_BLOCKS'
 
+//Getter and Setter Types for Product Groups
 export const GET_PRODUCT_GROUPS = 'GET_PRODUCT_GROUPS'
 export const SET_PRODUCT_GROUPS = 'SET_PRODUCT_GROUPS'
 
+//Getter and Setter Types for Products
 export const GET_PRODUCTS = 'GET_PRODUCTS'
 export const SET_PRODUCTS = 'SET_PRODUCTS'
 
+//Getter and Setter Types for Events
 export const GET_EVENTS = 'GET_EVENTS'
 export const SET_EVENTS = 'SET_EVENTS'
