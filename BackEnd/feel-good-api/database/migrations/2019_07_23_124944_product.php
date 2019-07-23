aefb1fce4e93ec00da4a43d5270cedbf3070df05
Kirky <re_kirky@hotmail.com>
2019-07-23 22:52:27 +1000

    Create 2019_07_23_124944_product.php
    Product table

---
+++ BackEnd/feel-good-api/database/migrations/2019_07_23_124944_product.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Product extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->integer('productid')->unique();
+            $table->string('name');
+            $table->string('type');
+            $table->string('size',20);
+            $table->string('summary');
+            $table->string('description');
+            $table->string('energy',20);
+            $table->string('fat',10);
+            $table->string('carbohydrate',10);
+            $table->string('protein',10);
+            $table->string('salt',10);
+            $table->string('ingredients');       
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
