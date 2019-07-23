a631234db8291952ac53d8a6a3780d3878063947
WestonCode <contact@westoncode.com>
2019-07-23 20:56:04 +1000

    Created Product Group Migration

---
+++ BackEnd/feel-good-api/database/migrations/2019_07_23_125039_create_product_group_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductGroupTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_groups', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->string('accentColor');
+            $table->string('tagLine');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_groups');
+    }
+}
