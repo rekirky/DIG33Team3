079ad2ade49a6e719152901bbf4f050cc6a97182
WestonCode <contact@westoncode.com>
2019-07-23 22:42:10 +1000

    Added Event and Product Seeder calls to Database Seeder

--- BackEnd/feel-good-api/database/seeds/DatabaseSeeder.php
+++ BackEnd/feel-good-api/database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call('ProductGroupSeeder');
+        $this->call('EventSeeder');
+        $this->call('ProductSeeder');
     }
 }
