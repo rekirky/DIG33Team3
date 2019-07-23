5357007bfa09cff4b57e632c27f6b0ebe5fefee8
WestonCode <contact@westoncode.com>
2019-07-23 22:40:59 +1000

    Added Product Group Seeder

--- BackEnd/feel-good-api/database/seeds/ProductGroupSeeder.php
+++ BackEnd/feel-good-api/database/seeds/ProductGroupSeeder.php
@@ -11,6 +11,16 @@ class ProductGroupSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \App\ProductGroup::insert([
+            "name" => "a bit bubbly",
+            "accentColor" => "#c0539f",
+            "tagLine" => "Enjoy our Bit Bubbly 100% natural* drinks with no added sugar. Now Available in 275ml paper bottles in a range of Feel Good flavours:"
+        ]);
+
+        \App\ProductGroup::insert([
+            "name" => "refreshingly still",
+            "accentColor" => "#f49b23",
+            "tagLine" => "Enjoy our Refreshingly Still 100% natural* drinks with no added sugar. Now Available in 275ml paper bottles in a range of Feel Good flavours:"
+        ]);
     }
 }
