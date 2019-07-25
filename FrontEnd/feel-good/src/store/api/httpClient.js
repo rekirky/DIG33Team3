6a9cb9d37e61b2cd447bdb1d73fd54ae48987a9d
WestonCode <contact@westoncode.com>
2019-07-25 18:40:20 +1000

    Updated backend connection URL

--- FrontEnd/feel-good/src/store/api/httpClient.js
+++ FrontEnd/feel-good/src/store/api/httpClient.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // axios instance
 // Axios handles the HTTP requests between the front end and the backend api
 const axiosInstance = axios.create({
-  baseURL: 'http://54.206.73.10',
+  baseURL: 'https://backend.endgame-feelgood.com',
   timeout: 30000
 })
 
