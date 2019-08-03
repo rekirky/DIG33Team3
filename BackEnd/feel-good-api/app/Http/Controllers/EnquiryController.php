553c47427d98adaead96392dee15a487b79390ed
WestonCode <contact@westoncode.com>
2019-08-03 18:51:27 +1000

    Added Enquiry Route and model

---
+++ BackEnd/feel-good-api/app/Http/Controllers/EnquiryController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Enquiry;
+use App\Http\Resources\Enquiry as EnquiryResource;
+
+class EnquiryController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $enquiry = new Enquiry;
+
+        $enquiry->name = $request->input('name');
+        $enquiry->email = $request->input('email');
+        $enquiry->message = $request->input('message');
+
+        $enquiry->save();
+
+        if($enquiry->save()){
+            return new EnquiryResource($enquiry);
+        }
+
+    }
+
+}
