4c3a0f029efb6afa5b555fd816c6fa96a6089269
Weston Code <contact@westoncode.com>
2019-08-05 10:30:56 +1000

    Removed Placeholder data references

--- FrontEnd/feel-good/src/store/api/index.js
+++ FrontEnd/feel-good/src/store/api/index.js
@@ -1,10 +1,7 @@
 import http from './httpClient'
-import * as placeholder from '../modules/testData'
 
 class Api {
   //Get Requests utilise the Axios Http Client to retreive selected data groups
-  //Placedholder JSON has been used in place of the axios connection until the backend application is completed
-  //Once Backend Application is complete the axios calls will be un commented and placeholders will be removed.
   async getContentBlocks (params) {
     let { data } = await http.get('/content-blocks', { params })
     return data
@@ -12,19 +9,16 @@ class Api {
 
   async getProductGroups (params) {
     let { data } = await http.get('/product-groups', { params })
-    //let data = placeholder.productGroups
     return data
   }
 
   async getEvents (params) {
     let { data } = await http.get('/events', { params })
-    //let data = placeholder.events
     return data
   }
 
   async getProducts (params) {
     let { data } = await http.get('/products', { params })
-    //let data = placeholder.products
     return data
   }
 
